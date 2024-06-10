af1e7ddda8c0583c433da450e93f8c4d2d4c6049
skylerYYa <skelanimalskitti@gmail.com>
2024-06-10 08:31:38 -0300

    PIZZARIA2A

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -3,6 +3,12 @@ const Inicio = () => {
     return (
         <>
             <h2>Página de Início</h2>
+            <p>Aula de ReactJS</p>
+            <input type="text" id="texto" />
+            <input type="button" id="botao"
+            value="CLIQUE AQUI"
+            onClick={()=>alert('Olá Mundo')}
+            />
         </>
     )
 }
